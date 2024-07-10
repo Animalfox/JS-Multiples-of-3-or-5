02278f277b49b3b1ae01387deada340055a8d81f
Animalfox <a72e@outlook.com>
2024-07-10 23:21:09 +0300

    Use solution function inside application

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
+import solution from "../src/solution.js";
+
 console.log('Started successfully');
+const input = 10;
+const actual = solution(input);
+console.log(`The result of function solution(10) is "${actual}"\n`);
